09b2a1194be90f4b599f58373e6192500977c0cd
thurberstudios <51497692+thurberstudios@users.noreply.github.com>
2019-07-25 01:13:54 -0500

    Add files via upload

---
+++ doc/vfm-admin/users/users.php
@@ -0,0 +1,16 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$L7mHSXtD$9TKIZ506dXZ8OH/bbXu0G.',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'EThurber',
+    'pass' => '$1$vAh4WJVz$tyfwgr7p/iOMwXdXoZhwn0',
+    'role' => 'user',
+  ),
+);
